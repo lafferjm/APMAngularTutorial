bc79bd41b59a732a1ef83ae89ce1857906c227c1
Justin Lafferty <lafferjm@gmail.com>
2017-04-09 09:29:36 -0500

    Refactoring changes.  Moved product information to separate module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductFilterPipe } from './product-list/product-filter.pipe';
-import { StarComponent } from './star/star.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-
-import { ProductService } from './product-list/product.service';
-import { ProductGuardService } from './product-detail/product-guard.service';
+import { ProductModule } from './product-list/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ProductFilterPipe,
-    StarComponent,
-    WelcomeComponent,
-    ProductDetailComponent
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpModule,
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { path: 'product/:id',
-        canActivate: [ ProductGuardService ], component: ProductDetailComponent },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ])
+    ]),
+    ProductModule
   ],
-  providers: [ ProductService, ProductGuardService ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
