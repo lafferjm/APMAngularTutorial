3ba9dca4d41a0a461abc1f9cbe268eee6d0d50c9
Justin Lafferty <lafferjm@gmail.com>
2017-04-08 21:18:51 -0500

    Routing with parameters, basics working.

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { IProduct } from '../product-list/product';
 
@@ -6,7 +7,14 @@ import { IProduct } from '../product-list/product';
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent  {
+export class ProductDetailComponent implements OnInit {
   pageTitle = 'Product Detail';
   product: IProduct;
+
+  constructor(private _route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const id = Number(this._route.snapshot.params['id']);
+    this.pageTitle += `: ${id}`;
+  }
 }
